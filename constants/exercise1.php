92aaa9471d170b0c5859c2ae0f768f300168dbc9
Natan <159673834+NatanLuz@users.noreply.github.com>
2026-07-28 21:58:26 -0300

    Mudando o comentario na parte de constants

--- constants/exercise1.php
+++ constants/exercise1.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
 // Exercise 1 - constants
-// Comentario: resolva o exercicio implementando a logica pedida.
+// Comentario: resolva o exercicio implementando a logica solicitada
 
 echo "Exercise 1 (constants) - TODO\n";
